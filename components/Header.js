2d787ff513b16dda9aff71e59c9bb74949f3f9f1
LeonYuAng3NT <leonzhang1996@hotmail.com>
2021-02-13 12:23:52 -0800

    Changed header

--- components/Header.js
+++ components/Header.js
@@ -141,7 +141,7 @@ class Header extends React.Component {
               color={nowTheme.COLORS.HEADER}
             />
             <Text style={{ fontFamily: 'montserrat-regular' }} size={16} style={styles.tabTitle}>
-              {optionLeft || 'Beauty'}
+              {optionLeft || 'Vegan'}
             </Text>
           </Block>
         </Button>
@@ -155,7 +155,7 @@ class Header extends React.Component {
               color={nowTheme.COLORS.HEADER}
             />
             <Text style={{ fontFamily: 'montserrat-regular' }} size={16} style={styles.tabTitle}>
-              {optionRight || 'Fashion'}
+              {optionRight || 'Non-Vegan'}
             </Text>
           </Block>
         </Button>
