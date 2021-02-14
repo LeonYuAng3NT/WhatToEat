932537858b89da31d102525b4782af58a424ec42
Xuhao Yu <xuhao@Xuhaos-MacBook-Pro.local>
2021-02-13 23:36:24 -0500

    backend flask setup

---
+++ server/flaskr/db.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    # g is special object unique for each request
+    # it is used to store data that might be accessed by multiple functions during the request
+    # similar to dbcp, if connection is 
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+# create a cli command to call the init_db function
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
